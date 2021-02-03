f0a897dfc088871dc963ee5121774ae986536b35
Ronan <30617930+MWRonanD@users.noreply.github.com>
2021-02-03 21:51:31 +0100

    cuboid start

---
+++ Assets/Scripts/VibrationImplementation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VibrationImplementation : Vibration
+{
+    bool vibrate = false;
+    float passedTime = 0;
+    public void VibPlus()
+    {
+        vibrate = true;
+    }
+
+    private void Update()
+    {
+        if (vibrate)
+        {
+            Handheld.Vibrate();
+        }   
+    }
+}
