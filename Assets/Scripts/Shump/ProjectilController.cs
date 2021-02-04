bddee1038edabea124a56f7c40ed12794885f1c6
Ronan <30617930+MWRonanD@users.noreply.github.com>
2021-02-04 22:58:48 +0100

    Shoot

---
+++ Assets/Scripts/Shump/ProjectilController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectilController : MonoBehaviour
+{
+    [SerializeField]
+    protected float speed = 20f;
+    protected float lifeTime = 2.5f;
+    protected float passedTime = 0;
+    Camera cam;
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+    private void Update()
+    {
+        passedTime += Time.deltaTime;
+        transform.position += cam.transform.forward * speed * Time.deltaTime;
+        if (passedTime >= lifeTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Shump/ShootController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootController : MonoBehaviour
+{
+    [SerializeField]
+    protected GameObject projectil;
+    [SerializeField]
+    protected Transform instancatePosition;
+    [Range(0.2f, 5f)]
+    [SerializeField]
+    protected float cooldown = 1;
+    protected float passedTimeFromLastShoot;
+    bool canShoot = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        passedTimeFromLastShoot = cooldown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (passedTimeFromLastShoot >= cooldown)
+            canShoot = true;
+        else
+            passedTimeFromLastShoot += Time.deltaTime;
+
+        if (canShoot && Input.GetButton("Fire1"))
+        {
+            passedTimeFromLastShoot = 0f;
+            canShoot = false;
+            GameObject p = Instantiate(projectil, instancatePosition.position, Quaternion.identity);
+        }
+    }
+}
