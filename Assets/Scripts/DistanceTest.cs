b94e7331984e0ee7d6fdf51aff8caf6f7c83c778
YumiD <gacon.dorian@gmail.com>
2021-02-05 20:26:35 +0100

    GameObject Distance

---
+++ Assets/Scripts/DistanceTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceTest : MonoBehaviour
+{
+    public GameObject[] pieces;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if (pieces == null)
+        pieces = GameObject.FindGameObjectsWithTag("Piece");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist;
+        if(pieces.Length>1){
+            for(int i=0; i<pieces.Length-1; i++){
+                for(int j=i+1; j<pieces.Length; j++){
+                    dist = Vector3.Distance(pieces[i].transform.position, pieces[j].transform.position);
+                    print("Distance to other: " + dist);
+                }
+            }
+        }
+    }
+}
