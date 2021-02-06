057cc7ddc605f833781a915fed72361fc0516d70
Ronan <30617930+MWRonanD@users.noreply.github.com>
2021-02-07 00:32:52 +0100

    score

---
+++ Assets/Scripts/Shmup/ScoreManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text scoreTxt;
+
+    private int score = 0;
+    public static ScoreManager  Instance { get; set; }
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    public void UpdateScore(int s)
+    {
+        score += s;
+        scoreTxt.text = score.ToString();
+    }
+}
---
+++ Assets/Scripts/Shmup/UiShmup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UiShmup : MonoBehaviour
+{
+    [SerializeField]
+    GameObject pauseMenu;
+    [SerializeField]
+    
+
+    public void TogglePause(bool b)
+    {
+        pauseMenu.SetActive(b);
+        Time.timeScale = b ? 0 : 1;
+    }
+
+    public void Quit()
+    {
+        //SceneManager.LoadScene(SOMEWHERE)
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
